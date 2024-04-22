50ba8f124a2d6aaa1f97f445eb3216bfbc56e312
Daniel <mail@danieleiler.net>
2024-04-22 22:49:39 +0200

    Fix

--- onboarding/dock.sh
+++ onboarding/dock.sh
@@ -1,22 +1,22 @@
 #!/bin/zsh --no-rcs
 set -x
 
-dockutil --remove 'FaceTime'
-dockutil --remove 'Kalender'
-dockutil --remove 'Kontakte'
-dockutil --remove 'Mail'
-dockutil --remove 'Freeform'
-dockutil --remove 'TV'
-dockutil --remove 'Musik'
-dockutil --remove 'Erinnerungen'
-dockutil --remove 'Notizen'
-dockutil --remove 'Karten'
-dockutil --add vnc://server.local --label 'VNC' --position end
-dockutil --add smb://server.local --label '(H) Home' --position end
-dockutil --add smb://server.local --label '(M) Marketing' --position end
-dockutil --add "/Applications/Microsoft Outlook.app" --position end
-dockutil --add "/Applications/Microsoft Teams (work or school).app" --position end
-dockutil --add "/Applications/Microsoft Word.app" --position end
-dockutil --add "/Applications/Microsoft Excel.app" --position end
-dockutil --add "/Applications/Microsoft PowerPoint.app" --position end
-dockutil --add "/Applications/Privileges.app" --position end
+/usr/local/bin/dockutil --remove 'FaceTime' --no-restart
+/usr/local/bin/dockutil --remove 'Kalender' --no-restart
+/usr/local/bin/dockutil --remove 'Kontakte' --no-restart
+/usr/local/bin/dockutil --remove 'Mail' --no-restart
+/usr/local/bin/dockutil --remove 'Freeform' --no-restart
+/usr/local/bin/dockutil --remove 'TV' --no-restart
+/usr/local/bin/dockutil --remove 'Musik' --no-restart
+/usr/local/bin/dockutil --remove 'Erinnerungen' --no-restart
+/usr/local/bin/dockutil --remove 'Notizen' --no-restart
+/usr/local/bin/dockutil --remove 'Karten' --no-restart
+/usr/local/bin/dockutil --add vnc://server.local --label 'VNC' --position end --no-restart
+/usr/local/bin/dockutil --add smb://server.local --label '(H) Home' --position end --no-restart
+/usr/local/bin/dockutil --add smb://server.local --label '(M) Marketing' --position end --no-restart
+/usr/local/bin/dockutil --add "/Applications/Microsoft Outlook.app" --position end --no-restart
+/usr/local/bin/dockutil --add "/Applications/Microsoft Teams (work or school).app" --position end --no-restart
+/usr/local/bin/dockutil --add "/Applications/Microsoft Word.app" --position end --no-restart
+/usr/local/bin/dockutil --add "/Applications/Microsoft Excel.app" --position end --no-restart
+/usr/local/bin/dockutil --add "/Applications/Microsoft PowerPoint.app" --position end --no-restart
+/usr/local/bin/dockutil --add "/Applications/Privileges.app" --position end
